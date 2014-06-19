b692ec6889196b171353edc846c598ec7011f04e
HakitoCZ <hakito.cz@gmail.com>
2014-06-19 21:26:22 +0100

    direction vector and TODO added

--- mathForGameDev.py
+++ mathForGameDev.py
@@ -5,6 +5,8 @@ Origin: playlist?list=PLW3Zl3wyJwWOpdhYedlD-yCB7WQoHf-My
 Used 2 space indent
 '''
 
+## TODO:
+##   Put all examples into functions
 
 # PacMan - P
 # Inky the Ghost - I
@@ -19,19 +21,27 @@ C = (2, -1)
 
 # Basic vector
 # IP - Inky to PacMan
+# IP = I - P
 # Pacman standing and Inky's distance enshortens
 IP = (P[0] - I[0], P[1] - I[1]) # (-1, -2)
 CP = (P[0] - C[0], P[1] - C[1]) # (-2, 0)
 
 # Distance
 # Vector length
-# no need to math.sqrt() - only to compare
+# To compare those two isn't math.sqrt() needed
 IP_dist = (IP[0]**2 + IP[1]**2) # 5
 CP_dist = (CP[0]**2 + CP[1]**2) # 4
 
 # Speed
 # End of vector isn't destination point but \
 #   the force by which it pulled
-IP_twice_fast = (IP[0]*2, IP[1]*2)
-IP_half_fast = (IP[0]/2, IP[1]/2)
+# NEED CHECK
+## IP_twice_fast = (IP[0]*2, IP[1]*2)
+## IP_half_fast = (IP[0]/2, IP[1]/2)
+## IP_twice_fast2 = IP_dist * 2
 
+# Looking direction
+# vector with length == 1
+# aka Normal length vectors
+# IP_dir =  (IP / PI_dist)
+IP_dir = (IP[0] / IP_dist, IP[1] / IP_dist)
